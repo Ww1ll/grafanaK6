66ea3a23ba855603eded349ca0b86592f8a76b60
Ww1ll <williambraz65@gmail.com>
2024-05-31 14:19:56 -0300

    feat: add dadosDinamicos

--- advancedtests.js
+++ advancedtests.js
@@ -13,13 +13,13 @@ export let options = {
 // agrupando diferentes casos de teste
 export default function () {
     group('Visit home page', function () {
-        http.get('htttps://test.k6.io')
+        http.get('https://test.k6.io')
         sleep(1)
     })
 
     group('Login and Browse', function() {
         let loginRes = http.post(
-            'htttps://test.k6.io/login',
+            'https://test.k6.io/login',
             { username: 'user', password: 'pass' }
         )
         sleep(1)
@@ -29,3 +29,42 @@ export default function () {
     sleep(1)
 
 }
+
+// uso de grupos e check
+import { group, check } from 'k6'
+
+export default function () {
+    group('Group 01',  function() {
+        let res = http.get('https://test.k6.io/')
+        check(
+            res,
+            { 'status was 200': (r) => r.status === 200 }
+        )
+    })
+
+
+    group('Group 02',  function() {
+        let res = http.get('https://test.k6.io/about')
+        check(
+            res,
+            { 'status was 200': (r) => r.status === 200 }
+        )
+    })
+
+}
+
+
+// para obeter uma captura de métricas mais detalhada
+
+import { Trend } from 'k6/metrics'
+
+let myTren = new Trend('custom_trend')
+
+export default function () {
+    let res = http.get('https://test.k6.io')
+    myTrend.add(res.timings.duration)
+
+}
+
+// após execução dos testes, para obter uma analisa detalhada
+    // k6 run --out json=test-results.json script.js
---
+++ dadosDinamicos.js
@@ -0,0 +1,17 @@
+// simulação dados reais
+
+export default function(){
+    let userId = Math.floor(Math.random() * 100)
+    let res = http.get(`https://test.k6.io/user/${userId}`)
+    check(res, { 'status was 200': (r) => r.status === 200 })
+}
+
+
+// integrando com api externa
+
+export default function(){
+    let apiKey = __ENV.API_KEY
+    let rest = http.get(`https://api.external.com/data?api_key=${apiKey}`)
+    check(res, { 'status was 200':  (r) => r.status === 200 })
+
+}
